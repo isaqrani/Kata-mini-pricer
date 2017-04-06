29e30933abbd617ee930d203e5d71565ab7a73bb
Ilyas SAQRANI <ilysaq@gmai.com>
2017-04-06 20:06:24 +0200

    Add gitignore file

--- src/test/java/org/mini/pricer/RandomVolatilityStrategyTest.java
+++ src/test/java/org/mini/pricer/RandomVolatilityStrategyTest.java
@@ -10,19 +10,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomVolatilityStrategyTest {
     private RandomVolatilityStrategy randomVolatilityStrategy;
-    private Double price;
     private Double volatility;
 
     @Before
-    public void initialise(){
+    public void initialise() {
         randomVolatilityStrategy = new RandomVolatilityStrategy();
         volatility = 1d;
     }
 
     @Test
-    public void should_generate_a_random(){
+    public void should_generate_a_random() {
         Double actualRandomVolatility = randomVolatilityStrategy.randomise(volatility);
         List<Double> expectedVolatilities = Arrays.asList(-1d, 0d, 1d);
         assertThat(actualRandomVolatility).isIn(expectedVolatilities);
     }
- }
+}
