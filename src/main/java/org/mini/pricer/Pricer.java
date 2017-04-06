aa632f3510df035825c0eb13bb62a7fb03288766
isaqrani <ilysaq@gmail.com>
2017-04-06 16:10:50 +0200

    Update Pricer.java

--- src/main/java/org/mini/pricer/Pricer.java
+++ src/main/java/org/mini/pricer/Pricer.java
@@ -2,18 +2,20 @@ package org.mini.pricer;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class Pricer {
     private List<LocalDate> holidays = Arrays.asList(LocalDate.of(2017,5,1), LocalDate.of(2017,5,8));
+    
     private VolatilityStrategy strategy;
 
     public Pricer(final VolatilityStrategy volatilityStrategy) {
         this.strategy = volatilityStrategy;
     }
 
-    public Double priceAt(final LocalDate startDate,final LocalDate targetedDate, Double price, Double volatility) {
+    public Double priceAt(final LocalDate startDate,final LocalDate targetedDate, final Double price,final Double volatility) {
 
         if(startDate.toEpochDay() == targetedDate.toEpochDay()){
             return price;
@@ -23,11 +25,11 @@ public class Pricer {
         }
     }
 
-    private LocalDate nextDay(LocalDate date) {
+    private LocalDate nextDay(final LocalDate date) {
         return skipWeekends(skipHolidays(date));
     }
 
-    private LocalDate skipWeekends(LocalDate date) {
+    private LocalDate skipWeekends(final LocalDate date) {
         if(date.getDayOfWeek().equals(DayOfWeek.FRIDAY)){
             return date.plusDays(3);
         }else if (date.getDayOfWeek().equals(DayOfWeek.SATURDAY)){
@@ -37,7 +39,7 @@ public class Pricer {
         }
     }
 
-    private LocalDate skipHolidays(LocalDate date){
+    private LocalDate skipHolidays(final  LocalDate date){
         if(holidays.contains(date)){
             return date.plusDays(1);
         }
