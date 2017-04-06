392578a632a25206f37be8451b02c85b02082aa0
isaqrani <ilysaq@gmail.com>
2017-04-06 17:54:42 +0200

    Update RandomVolatilityStrategyTest.java

--- src/test/java/org/mini/pricer/RandomVolatilityStrategyTest.java
+++ src/test/java/org/mini/pricer/RandomVolatilityStrategyTest.java
@@ -3,6 +3,9 @@ package org.mini.pricer;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomVolatilityStrategyTest {
