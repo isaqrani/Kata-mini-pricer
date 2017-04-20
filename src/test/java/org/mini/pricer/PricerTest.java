94fe76779ee3692c303a7b13479a9df64ca570ed
isaqrani <ilysaq@gmail.com>
2017-04-20 16:56:55 +0200

    Update PricerTest.java

--- src/test/java/org/mini/pricer/PricerTest.java
+++ src/test/java/org/mini/pricer/PricerTest.java
@@ -2,9 +2,12 @@ package org.mini.pricer;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import org.mini.pricer.volatility.DeterministicVolatilityStrategy;
 import org.mini.pricer.volatility.VolatilityStrategy;
+
 import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PricerTest {
@@ -100,5 +103,4 @@ public class PricerTest {
 
         assertThat(calculatedPrice).isEqualTo(99);
     }
-
 }
